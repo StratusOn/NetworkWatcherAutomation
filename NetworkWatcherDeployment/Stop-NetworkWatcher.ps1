7e1e598927eb80135c5d5e87db7d0c97e5eeafb8
Raouché <mohammad.jalloul@outlook.com>
2018-03-14 02:42:40 -0700

    Updated Start-NetworkWatcher script and added Stop-NetworkWatcher script.

--- NetworkWatcherDeployment/Stop-NetworkWatcher.ps1
+++ NetworkWatcherDeployment/Stop-NetworkWatcher.ps1
@@ -1,7 +1,7 @@
 #Requires -Version 5.0
 <#
     .DESCRIPTION
-    This runbook is intended to start Azure Network Watcher on a list of VMs . 
+    This runbook is intended to stop Azure Network Watcher that is running on a list of VMs . 
     
     This runbook requires MSI to be enabled on an Azure Function using https://github.com/StratusOn/MSI-GetToken-FunctionAppand an MSI URL to be provided.
 
@@ -14,16 +14,8 @@
     - TenantId: The tenant id.
     - SubscriptionId: The subscription id.
     - MsiGetDefaultTokenEndpoint: The MSI endpoint URL for retrieving an access token.
-    - MaxCaptureTimeInMinutes: The maximum time in minutes for a packet capture.
-    - NetworkWatcherStorageAccountName: The name of the Azure Storage Account where the Network Watcher packet capture logs will be written.
-    - NetworkWatcherStorageAccountResourceGroupName: The name of the Resource Group that contains the Azure Storage Account where the Network Watcher packet capture logs will be written.
-    - PacketCaptureFilterProtocol: The protocol of the packet capture filter. This can be either UDP or TCP.
-    - PacketCaptureFilterRemoteIpAddress: The remote IP address of the packet capture filter. This can be a single IP address or a range. e.g. 1.1.1.1-255.255.255.255.
-    - PacketCaptureFilterRemotePort: The remote port of the packet capture filter. This can be a single number or semi-colon separated port numbers. e.g. 20;80;443.
-    - PacketCaptureFilterLocalIpAddress: The local IP address of the packet capture filter. This can be a single IP address or a range. e.g. 1.1.1.1-255.255.255.255.
-    - PacketCaptureFilterLocalPort: The local port of the packet capture filter. This can be a single number or a port range. e.g. 1-65535.
 #>
-workflow Start-NetworkWatcher {
+workflow Stop-NetworkWatcher {
     function Get-AccessToken {
         param(
             [string] $MsiGetDefaultTokenEndpoint,
@@ -116,9 +108,8 @@ workflow Start-NetworkWatcher {
         while(!$success)
     }
 
-    function Start-NetworkWatcherForVm {
-        param($VmName, $ResourceGroupName, $NetworkWatcherStorageAccountName, $NetworkWatcherStorageAccountResourceGroupName, $MaxCaptureTimeInMinutes,
-              $PacketCaptureFilterProtocol, $PacketCaptureFilterRemoteIpAddress, $PacketCaptureFilterRemotePort, $PacketCaptureFilterLocalIpAddress, $PacketCaptureFilterLocalPort)
+    function Stop-NetworkWatcherForVm {
+        param($VmName, $ResourceGroupName)
     
         $ErrorActionPreference = 'Stop'
 
@@ -141,9 +132,10 @@ workflow Start-NetworkWatcher {
             if (-not ($vm.Statuses[1].Code -like "*running*"))
             {
                 $vmStatus = $vm.Statuses[1].DisplayStatus
-                $message = "[VM '$VmName']: Skipping VM since it is not in running state (status: $vmStatus). Start the VM and then resume this runbook."
+                $message = "[VM '$VmName']: Skipping VM since it is not in running state (status: $vmStatus)."
                 echo $message
-                throw $message
+                Write-Warning $message
+                Return
             }
 
             # Get the VM properties, including the VM ID.
@@ -158,41 +150,37 @@ workflow Start-NetworkWatcher {
                 $nw = Get-AzureRmResource | Where {$_.ResourceType -eq "Microsoft.Network/networkWatchers" -and $_.Location -eq "$region" }
                 if ($nw -eq $null)
                 {
-                    $message = "[VM '$VmName']: Skipping VM since Network Watcher was not found enabled in region '$region'. Enable Network Watcher for the region and then resume this runbook."
+                    $message = "[VM '$VmName']: Skipping VM since Network Watcher was not found enabled in region '$region'."
                     echo $message
-                    throw $message
+                    Write-Warning $message
+                    Return
                 }
 
                 echo "[VM '$VmName']: Found Network Watcher enabled for VM in '$region'."
                 $networkWatcher = Get-AzureRmNetworkWatcher -Name $nw.Name -ResourceGroupName $nw.ResourceGroupName
-                $storageAccount = Get-AzureRmStorageAccount -ResourceGroupName $NetworkWatcherStorageAccountResourceGroupName -Name $NetworkWatcherStorageAccountName
-                $filter1 = New-AzureRmPacketCaptureFilterConfig -Protocol $PacketCaptureFilterProtocol -RemoteIPAddress "$PacketCaptureFilterRemoteIpAddress" -RemotePort "$PacketCaptureFilterRemotePort" -LocalIPAddress "$PacketCaptureFilterLocalIpAddress" -LocalPort "$PacketCaptureFilterLocalPort"
-                $formattedDateTime = Get-Date -format "yyyyMMdd-hhmmss"
-                $packetCaptureName = "PacketCapture-$VmName-$formattedDateTime"
-                $timeLimitInMinutes = [int]$MaxCaptureTimeInMinutes
-                $timeLimitInSeconds = $timeLimitInMinutes*60
-                $packetCapture = New-AzureRmNetworkWatcherPacketCapture -NetworkWatcher $networkWatcher -TargetVirtualMachineId $vm.Id -PacketCaptureName $packetCaptureName -StorageAccountId $storageAccount.id -TimeLimitInSeconds $timeLimitInSeconds -Filter $filter1
+                $packetCapture = Get-AzureRmNetworkWatcherPacketCapture -NetworkWatcher $networkWatcher | Where {$_.Target -eq $vm.Id} | Where {$_.PacketCaptureStatus -eq "Running" -or $_.PacketCaptureStatus -eq $null}
                 if ($packetCapture -eq $null)
                 {
-                    $message = "[VM '$VmName']: Could not start Network Watcher capture '$packetCaptureName' on VM. If a capture with the same name already exists, delete the capture and resume this runbook."
+                    $message = "[VM '$VmName']: Could not find a Network Watcher capture in a Running state on VM."
                     echo $message
-                    throw $message
+                    Write-Warning $message
                 }
                 else
                 {
-                    $message = "[VM '$VmName']: Started Network Watcher capture '$packetCaptureName' on VM. The capture will stop after $MaxCaptureTimeInMinutes minutes. This runbook can be stopped to end the capture at any time."
+                    $packetCaptureName = $packetCapture.Name
+                    $message = "[VM '$VmName']: Found Network Watcher capture '$packetCaptureName' on VM."
                     echo $message
 
-                    #Write-Verbose "[VM '$VmName'] Packet Capture ($packetCaptureName):"
-                    #Write-Verbose $packetCapture
-                    #$packetCapture = Get-AzureRmNetworkWatcherPacketCapture -NetworkWatcher $networkWatcher -PacketCaptureName $packetCaptureName
+                    Stop-AzureRmNetworkWatcherPacketCapture -NetworkWatcher $networkWatcher -PacketCaptureName "$packetCaptureName"
+                    $message = "[VM '$VmName']: Stopped Network Watcher capture '$packetCaptureName' on VM."
+                    echo $message
                 }
             }
             else
             {
-                $message = "[VM '$VmName']: Skipping VM since it does not have the Network Watcher extension and agent installed. Install VM extension and then resume this runbook."
+                $message = "[VM '$VmName']: Skipping VM since it does not have the Network Watcher extension and agent installed."
                 echo $message
-                throw $message
+                Write-Warning $message
             }
         }
         else
@@ -204,7 +192,7 @@ workflow Start-NetworkWatcher {
     }
 
     #
-    # Start_NetworkWatcher.ps1
+    # Stop_NetworkWatcher.ps1
     #
 
     $ErrorActionPreference = 'Stop'
@@ -214,7 +202,6 @@ workflow Start-NetworkWatcher {
     #Get-ChildItem Env:
 
     $currentTime = (Get-Date).ToUniversalTime()
-    $maxCaptureTimeInMinutesUpperLimit = 300 # No more than 5 hours allowed. If this value is changed to more than 5 hours, Network Watcher will return an error.
     $maxRetryCount = 5
     $retryIntervalInSeconds = 30
 
@@ -226,28 +213,12 @@ workflow Start-NetworkWatcher {
     $tenantId = Get-AutomationVariable -Name 'TenantId'
     $subscriptionId = Get-AutomationVariable -Name 'SubscriptionId'
     $msiGetDefaultTokenEndpoint = Get-AutomationVariable -Name 'MsiGetDefaultTokenEndpoint'
-    $maxCaptureTimeInMinutes = Get-AutomationVariable -Name 'MaxCaptureTimeInMinutes'
-    $networkWatcherStorageAccountName = Get-AutomationVariable -Name 'NetworkWatcherStorageAccountName'
-    $networkWatcherStorageAccountResourceGroupName = Get-AutomationVariable -Name 'NetworkWatcherStorageAccountResourceGroupName'
-    $packetCaptureFilterProtocol = Get-AutomationVariable -Name 'PacketCaptureFilterProtocol'
-    $packetCaptureFilterRemoteIpAddress = Get-AutomationVariable -Name 'PacketCaptureFilterRemoteIpAddress'
-    $packetCaptureFilterRemotePort = Get-AutomationVariable -Name 'PacketCaptureFilterRemotePort'
-    $packetCaptureFilterLocalIpAddress = Get-AutomationVariable -Name 'PacketCaptureFilterLocalIpAddress'
-    $packetCaptureFilterLocalPort = Get-AutomationVariable -Name 'PacketCaptureFilterLocalPort'
     
     echo "-- Target Resource Groups: $targetResourceGroup"
     echo "-- Target Virtual Machines: $targetVirtualMachines"
     echo "-- Tenant Id: $tenantId"
     echo "-- Subscription Id: $subscriptionId"
     echo "-- MSI Get Default Token Endpoint: $msiGetDefaultTokenEndpoint"
-    echo "-- Max Capture Time In Minutes: $maxCaptureTimeInMinutes"
-    echo "-- Network Watcher Storage Account Name: $networkWatcherStorageAccountName"
-    echo "-- Network Watcher Storage Account Resource Group Name: $networkWatcherStorageAccountResourceGroupName"
-    echo "-- Network Watcher Filter Protocol: $packetCaptureFilterProtocol"
-    echo "-- Network Watcher Filter Remote IP Address: $packetCaptureFilterRemoteIpAddress"
-    echo "-- Network Watcher Filter Remote Port: $packetCaptureFilterRemotePort"
-    echo "-- Network Watcher Filter Local IP Address: $packetCaptureFilterLocalIpAddress"
-    echo "-- Network Watcher Filter Local Port: $packetCaptureFilterLocalPort"
 
     # Variables sanity checks.
     if ($targetResourceGroup -eq $null -or $targetResourceGroup -eq '')
@@ -280,33 +251,6 @@ workflow Start-NetworkWatcher {
         Write-Error $message -ErrorAction Stop
     }
 
-    if ($maxCaptureTimeInMinutes -le 0)
-    {
-        $message = "MaxCaptureTimeInMinutes must be greater than 0 and less or equal to $maxCaptureTimeInMinutesUpperLimit minutes."
-        Write-Error $message -ErrorAction Stop
-    }
-    else
-    {
-        if ($maxCaptureTimeInMinutes -gt $maxCaptureTimeInMinutesUpperLimit)
-        {
-            $message = "MaxCaptureTimeInMinutes cannot be greater than $maxCaptureTimeInMinutesUpperLimit minutes. Forcing current value ($maxCaptureTimeInMinutes) to $maxCaptureTimeInMinutesUpperLimit."
-            Write-Warning $message
-            $maxCaptureTimeInMinutes = $maxCaptureTimeInMinutesUpperLimit
-        }
-    }
-
-    if ($networkWatcherStorageAccountName -eq $null -or $networkWatcherStorageAccountName -eq '')
-    {
-        $message = "NetworkWatcherStorageAccountName not specified."
-        Write-Error $message -ErrorAction Stop
-    }
-
-    if ($networkWatcherStorageAccountResourceGroupName -eq $null -or $networkWatcherStorageAccountResourceGroupName -eq '')
-    {
-        $message = "NetworkWatcherStorageAccountResourceGroupName not specified."
-        Write-Error $message -ErrorAction Stop
-    }
-
     # Get the access token.
     $accessToken = Get-AccessToken -MsiGetDefaultTokenEndpoint $msiGetDefaultTokenEndpoint -MaxRetryCount $maxRetryCount -RetryIntervalInSeconds $retryIntervalInSeconds
     Write-Verbose "Access Token: $accessToken"
@@ -315,7 +259,7 @@ workflow Start-NetworkWatcher {
     Authenticate-AccessToken -AccessToken $accessToken -SubscriptionId $subscriptionId -MaxRetryCount $maxRetryCount -RetryIntervalInSeconds $retryIntervalInSeconds -ErrorAction Stop
 
     $startTime = Get-Date
-    # Start a packet capture for each VM specified:
+    # Stop the packet capture on each VM specified:
     $vmNames = $targetVirtualMachines -split ','
 
     $errorMessages = ""
@@ -323,9 +267,7 @@ workflow Start-NetworkWatcher {
     {
         try
         {
-            Start-NetworkWatcherForVm -VmName $vmName -ResourceGroupName $targetResourceGroup -NetworkWatcherStorageAccountName $networkWatcherStorageAccountName -NetworkWatcherStorageAccountResourceGroupName $networkWatcherStorageAccountResourceGroupName -MaxCaptureTimeInMinutes $maxCaptureTimeInMinutes `
-                                      -PacketCaptureFilterProtocol $packetCaptureFilterProtocol -PacketCaptureFilterRemoteIpAddress $packetCaptureFilterRemoteIpAddress -PacketCaptureFilterRemotePort $packetCaptureFilterRemotePort -PacketCaptureFilterLocalIpAddress $packetCaptureFilterLocalIpAddress -PacketCaptureFilterLocalPort $packetCaptureFilterLocalPort
-            #Start-NetworkWatcherForVm -VmName $vmName -ResourceGroupName $targetResourceGroup -NetworkWatcherStorageAccountName $networkWatcherStorageAccountName -NetworkWatcherStorageAccountResourceGroupName $networkWatcherStorageAccountResourceGroupName -MaxCaptureTimeInMinutes $maxCaptureTimeInMinutes
+            Stop-NetworkWatcherForVm -VmName $vmName -ResourceGroupName $targetResourceGroup
         }
         catch
         {
@@ -336,11 +278,11 @@ workflow Start-NetworkWatcher {
     }
 
     $endTime = Get-Date
-    echo ("Starting Network Watcher jobs took {0}." -f ($endTime - $startTime))
+    echo ("Stopping Network Watcher jobs took {0}." -f ($endTime - $startTime))
 
     if ($errorMessages -ne "")
     {
-        Write-Error "Error(s) occurred while starting network watchers:`n$errorMessages" -ErrorAction Stop
+        Write-Error "Error(s) occurred while stopping network watchers:`n$errorMessages" -ErrorAction Stop
     }
 
     # End of runbook
